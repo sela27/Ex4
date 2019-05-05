120a8a829727e2c910774b20c95cd1c72d55ad1c
sela27 <sela@goldenberg.xyz>
2019-05-05 15:14:13 +0300

    First try part B Guesser.hpp

--- Guesser.hpp
+++ Guesser.hpp
@@ -1,31 +1,46 @@
 #pragma once
+
 #include<iostream>
-#include <string>
-#include "calculate.hpp"
-
 using namespace std;
+#include <string>
+namespace bullpgia
 
-namespace bullpgia 
 {
-	class Guesser 
+	class Guesser
 	{
-		protected:
-			int length;
-			int lastbull;
-			int lastpgia;
-		public:
-			virtual string guess() = 0;
-			virtual void startNewGame(int l)
-			{
-				length = l;
-			}
-			virtual void learn(string reply)
-			{
-				string s1 = reply.substr(0, reply.find(","));
-				string s2 = reply.substr(reply.find(",") + 1);
-				lastbull = stoi(s1);
-				lastpgia = stoi(s2);
-			}
-	};
+	protected:
 
+		int length;
+		int lastbull;
+		int lastpgia;
+		bool NewGame;
+
+	public:
+		Guesser()
+		{
+			lastbull = -1;
+			lastpgia = -1;
+		}
+		virtual ~Guesser()
+
+		{
+
+		}
+		virtual string guess() = 0;
+		void startNewGame(int l)
+
+		{
+			length = l;
+			NewGame = true;
+
+		}
+		virtual void learn(string reply)
+		{
+			string s1 = reply.substr(0, reply.find(","));
+			string s2 = reply.substr(reply.find(",") + 1);
+			lastbull = stoi(s1);
+			lastpgia = stoi(s2);
+		}
+
+	};
 }
