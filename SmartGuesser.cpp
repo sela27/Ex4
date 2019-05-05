8620bccc4ef3e4cb9ad8cd0ddf124b85796c76c8
sela27 <sela@goldenberg.xyz>
2019-05-05 15:17:06 +0300

    First try part B SmartGuesser.cpp

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -3,36 +3,47 @@
 #include<string>
 using namespace std;
 
-string SmartGuesser::guess() 
-{
-	
-	return "0000";	
-	/*if (this->s == "0000") 
-	{
+string SmartGuesser::guess() {
+
+	if (this->NewGame) {
 		int i;
 		string Temperory_str;
-		for(i=0;i<this->length;i++)
+		this->s = "";
+		this->lastbull = -1;
+		this->lastpgia = -1;
+		this->Memory_bull = -1;
+		this->index = 0;
+		this->NewGame = false;
+		for (i = 0; i < this->length; i++)                    /////what is length ???
 		{
-			Temperory_str.at(i) = s.at(i);
+			Temperory_str.append("0");
 		}
-		s = Temperory_str;
-
+		this->s.append(Temperory_str);
+		return s;
 	}
-	
-	
-		if ((this->Memory_Pgia == this->lastpgia) && this->length>=s.length()) {
-			s.at(this->index) = s.at(this->index) + 1;
 
-			return s;
-		}
-	    
 
-		if ((this->Memory_Pgia + 1 == this->lastpgia) && (this->length >= s.length())) {
-			this->Memory_Pgia++;
-			this->index = this->index + 1;
-			s.at(this->index) = s.at(this->index) + 1;
+	if (((this->Memory_bull == this->lastbull)) || this->Memory_bull == -1 || this->last_drop) {
+		s.at(this->index) = s.at(this->index) + 1;
+		this->last_drop = false;
+		return s;
+	}
 
-			return s;
-		}*/
-		
+
+	else if ((this->Memory_bull + 1 == this->lastbull)) {
+		//this->Memory_bull++;
+		this->index = this->index + 1;
+		s.at(this->index) = s.at(this->index) + 1;
+
+		return s;
+	}
+
+	else
+	{
+		s.at(this->index) = s.at(this->index) - 1;
+		this->index = this->index + 1;
+		this->last_drop = true;
+		return s;
+	}
+
 }
