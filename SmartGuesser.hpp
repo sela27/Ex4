67e7d3ef506906344eb22bc4a3231999ece59835
sela27 <sela@goldenberg.xyz>
2019-05-14 07:01:06 +0300

    Update SmartGuesser.hpp
    adding comments

--- SmartGuesser.hpp
+++ SmartGuesser.hpp
@@ -4,14 +4,20 @@ using namespace std;
 
 class SmartGuesser : public bullpgia::Guesser {
 public:
+
 	string s = "";
 	int Memory_bull = -1;
 	int index = 0;
 	bool last_drop = false;
 	void learn(string reply) override
+
 	{
+
 		Memory_bull = this->lastbull;
-		Guesser::learn(reply);
+		Guesser::learn(reply);                           //// go and tell me how much bulls and pgia
+
 	}
+
 	string guess() override;
+
 };
